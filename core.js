034c1b80131d9f77ad13bc57b7c7559de4e96a51
Robert M Ochshorn <mail@RMOZONE.COM>
2012-09-09 00:48:09 +0200

    Refactor to share a single socket.io connection throughout submodules

--- core.js
+++ core.js
@@ -1,19 +1,46 @@
 var http = require('http')
+  , app = http.createServer(handle)
   , url = require('url')
-  , fs = require('fs');
+  , fs = require('fs')
+  , io = require('socket.io').listen(app)
+  , rooter = require('rooter').rooter
+  , submodules = {}
+  , ctx = {rooter: rooter, io: io}; // Passed to all handle() calls
 
-var app = http.createServer(handle);
+/* Returns a rooter-aware submodule. The first time a submodule is
+ * loaded, an init() is called (if it exists).
+ */
+function get_submodule(name) {
+    if(!(name in submodules)) {
+        try {
+            var submodule = require(name);
+            if(!submodule.handle) {
+                console.log("Warning: ", name, " does not implement `handle,' ignoring.");
+                return false;
+            }
+            submodules[name] = submodule;
+            if(submodules[name].init){
+                submodules[name].init(ctx);
+            }
+        }
+        catch (e) {
+            console.log(e);
+            return false;
+        }
+    }
+
+    return submodules[name];
+}
 
 function handle (req, res) {
     var req_url = url.parse(req.url, true);
 
-    var module = req_url.path.split('/')[1];
+    var module = get_submodule(req_url.path.split('/')[1]);
 
-    try {
-        return require(module).handle(req, res);
+    if(module) {
+        return module.handle(req, res, ctx);
     }
-    catch (e) {
-        console.log(e);
+    else {
         res.writeHead(404);
         res.end('Not found');
     }
